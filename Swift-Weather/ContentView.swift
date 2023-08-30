ed943acb5118791cc5a59fe6238bdb45dd618ea5
Shahiru Roshen <68189195+Shahiru93@users.noreply.github.com>
2023-08-30 12:17:45 +0530

    first commit

---
+++ Swift-Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-Weather
+//
+//  Created by Shahiru Roshen on 2023-03-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.blue)
+                .edgesIgnoringSafeArea(.all)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-Weather/Swift_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_WeatherApp.swift
+//  Swift-Weather
+//
+//  Created by Shahiru Roshen on 2023-03-16.
+//
+
+import SwiftUI
+
+@main
+struct Swift_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
